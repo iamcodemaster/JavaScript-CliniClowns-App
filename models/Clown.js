136c21005cdfbe8c152310c2c1cc981414a26d71
robertvaida@hotmail.com <robertvaida@hotmail.com>
2019-03-01 15:42:36 +0100

    config

--- models/Clown.js
+++ models/Clown.js
@@ -1,14 +1,14 @@
-// 'use strict';
-// module.exports = (sequelize, DataTypes) => {
-//   const Clown = sequelize.define('Clown', {
-//     nickname: DataTypes.STRING,
-//     firstName: DataTypes.STRING,
-//     lastName: DataTypes.STRING,
-//     email: DataTypes.STRING,
-//     password: DataTypes.STRING
-//   }, {});
-//   Clown.associate = function(models) {
-//     // associations can be defined here
-//   };
-//   return Clown;
-// };
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Clown = sequelize.define('Clown', {
+    nickname: DataTypes.STRING,
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {});
+  Clown.associate = function(models) {
+    // associations can be defined here
+  };
+  return Clown;
+};
