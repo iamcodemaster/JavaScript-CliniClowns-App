d5ed827b02b44832a31cc3dac94ccf2befc33c54
Emily <emily.berghen@hotmail.com>
2019-03-10 20:02:18 +0100

    Added background map & first tent

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -6,13 +6,21 @@ class Home extends Component {
     render () {
         return (
             <div className="container">
+            <div class="background">
+                <img src="/assets/map/map.png" alt="achtergrond"></img>
+            </div>
             <header>
                 <a href="/" class="logo">
                     <p>Home</p>
                 </a>
                 <Navbar />
             </header>
-                <div>
+                <div class="map">
+                    <div class="map-element" id="clowns">
+                        <a href="/clowns">
+                            <img src="/assets/map/clownstent.png" alt="ontdek de clowns"></img>
+                        </a>
+                    </div>
                 </div>
             </div>
         )
