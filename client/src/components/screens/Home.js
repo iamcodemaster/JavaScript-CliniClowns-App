529501c44726e6012d70a03426fd2f65b6b8b2f6
Emily <emily.berghen@hotmail.com>
2019-03-12 22:42:50 +0100

    first attempt positioning for mobile (far from perfect.. sadly)

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -5,7 +5,7 @@ import './Home.css'
 class Home extends Component {
     render () {
         return (
-            <div className="container">
+            <div className="container home">
             <div class="background">
                 <img src="/assets/map/map.png" alt="achtergrond"></img>
             </div>
@@ -18,7 +18,37 @@ class Home extends Component {
                 <div class="map">
                     <div class="map-element" id="clowns">
                         <a href="/clowns">
-                            <img src="/assets/map/clownstent.png" alt="ontdek de clowns"></img>
+                            <img src="/assets/map/clowns.png" alt="ontdek de clowns"></img>
+                        </a>
+                    </div>
+                    <div class="map-element" id="tipi">
+                        <a href="/clowns">
+                            <img src="/assets/map/tipi.png" alt="weet ik nog nie"></img>
+                        </a>
+                    </div>
+                    <div class="map-element" id="airballoon">
+                        <a href="/clowns">
+                            <img src="/assets/map/airballoon.png" alt="ontdek de clowns"></img>
+                        </a>
+                    </div>
+                    <div class="map-element" id="caravan">
+                        <a href="/clowns">
+                            <img src="/assets/map/caravan.png" alt="ontdek de clowns"></img>
+                        </a>
+                    </div>
+                    <div class="map-element" id="parasol">
+                        <a href="/clowns">
+                            <img src="/assets/map/parasol.png" alt="ontdek de clowns"></img>
+                        </a>
+                    </div>
+                    <div class="map-element" id="tent">
+                        <a href="/clowns">
+                            <img src="/assets/map/tent.png" alt="ontdek de clowns"></img>
+                        </a>
+                    </div>
+                    <div class="map-element" id="sign">
+                        <a href="/">
+                            <img src="/assets/map/sign.png" alt="terug naar boven"></img>
                         </a>
                     </div>
                 </div>
